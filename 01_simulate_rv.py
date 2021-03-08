dfd025e33ee7906ba692e8b09063a96e97c58cb8
Finanzas Cuantitativas FC <69920097+finanzascuantitativasfc@users.noreply.github.com>
2021-03-08 10:40:14 -0600

    Add files via upload

---
+++ 01_simulate_rv.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 09:22:02 2021
+
+@author: Meva
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import scipy
+import importlib
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2
+
+print('Hello World!')
+
+nb_sims = 10**6
+df = 500
+dist_name = 'normal' # student normal exponential uniform chi-square
+dist_type = 'simulated RV' # real custom
+
+
+if dist_name == 'normal':
+    x = np.random.standard_normal(nb_sims)
+    x_description = dist_type + ' ' + dist_name
+elif dist_name == 'exponential':
+    x = np.random.standard_exponential(nb_sims)
+    x_description = dist_type + ' ' + dist_name
+elif dist_name == 'uniform':
+    x = np.random.uniform(0,1,nb_sims)
+    x_description = dist_type + ' ' + dist_name
+elif dist_name == 'student':
+    x = np.random.standard_t(df=df, size=nb_sims)
+    x_description = dist_type + ' ' + dist_name + ' | df = ' + str(df)
+elif dist_name == 'chi-square':
+    x = np.random.chisquare(df=df, size=nb_sims)
+    x_description = dist_type + ' ' + dist_name + ' | df = ' + str(df)
+    
+'''
+Goal: create a Jarque-Bera normality test
+'''
+
+x_mean = np.mean(x)
+x_std = np.std(x)
+x_skew = skew(x)
+x_kurtosis = kurtosis(x) # excess kurtosis
+x_jb_stat = nb_sims/6*(x_skew**2 + 1/4*x_kurtosis**2)
+x_p_value = 1 - chi2.cdf(x_jb_stat, df=2)
+x_is_normal = (x_p_value > 0.05) # equivalently jb < 6
+
+# jb_list = []
+# jb_list.append(x_jb_stat)
+
+print('skewness is ' + str(x_skew))
+print('kurtosis is ' + str(x_kurtosis))
+print('JB statistic is ' + str(x_jb_stat))
+print('p-value ' + str(x_p_value))
+print('is normal ' + str(x_is_normal))
+
+
+# plot histogram
+plt.figure()
+plt.hist(x,bins=100)
+plt.title(x_description)
+plt.show()
+
---
+++ 02_market_data.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 09:33:38 2021
+
+@author: Meva
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import scipy
+import importlib
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2
+
+
+# get market data
+# remember to modify the path to match your own directory
+directory = 'C:\\Users\Meva\\.spyder-py3\\data\\2021-2\\'
+# inputs
+ric = '^STOXX50E' # BBVA.MC MXN=X ^STOXX50E
+path = directory + ric + '.csv' 
+raw_data = pd.read_csv(path)
+
+# # use prices
+# x = raw_data['Close'].values
+# x_description = 'market data ' + ric
+# nb_rows = len(x)
+
+# create table of returns
+t = pd.DataFrame()
+t['date'] = pd.to_datetime(raw_data['Date'], dayfirst=True)
+t['close'] = raw_data['Close']
+t.sort_values(by='date', ascending=True)
+t['close_previous'] = t['close'].shift(1)
+t['return_close'] = t['close']/t['close_previous'] - 1
+t = t.dropna()
+t = t.reset_index(drop=True)
+
+x = t['return_close'].values
+x_description = 'market data ' + ric
+nb_rows = len(x)
+
+    
+'''
+Goal: create a Jarque-Bera normality test
+'''
+
+x_mean = np.mean(x)
+x_std = np.std(x)
+x_skew = skew(x)
+x_kurtosis = kurtosis(x) # excess kurtosis
+x_jb_stat = nb_rows/6*(x_skew**2 + 1/4*x_kurtosis**2)
+x_p_value = 1 - chi2.cdf(x_jb_stat, df=2)
+x_is_normal = (x_p_value > 0.05) # equivalently jb < 6
+
+# jb_list = []
+# jb_list.append(x_jb_stat)
+
+print('skewness is ' + str(x_skew))
+print('kurtosis is ' + str(x_kurtosis))
+print('JB statistic is ' + str(x_jb_stat))
+print('p-value ' + str(x_p_value))
+print('is normal ' + str(x_is_normal))
+
+
+# plot histogram
+plt.figure()
+plt.hist(x,bins=50)
+plt.title(x_description)
+plt.show()
